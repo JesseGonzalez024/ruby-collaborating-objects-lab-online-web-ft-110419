1b5d0d463fe29a64d85a80d8b2814565d750d012
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-22 04:28:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+  def self.all 
+    @@all
+  end
+  def self.new_by_filename(file)
+    array = file.split(" - ")
+    song = Song.new(array[1])
+    artist = Artist.find_or_create_by_name(array[0])
+    
+    
+  end
+  
+ 
+ 
+ 
+ 
+end
