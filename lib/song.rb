eb4a21ce1c9b29bda2ceef6091df7d2ab81cc202
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-22 04:35:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
   def self.new_by_filename(file)
     array = file.split(" - ")
     song = Song.new(array[1])
-    artist = Artist.find_or_create_by_name(array[0])
+    # artist = Artist.find_or_create_by_name(array[0])
     
     
   end
